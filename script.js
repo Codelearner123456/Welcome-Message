95e7fb38912460655646a6413a2b0794465a7242
Adedmidun <154547213+Codelearner123456@users.noreply.github.com>
2025-05-08 18:00:02 +0100

    changed script.js

--- script.js
+++ script.js
@@ -4,9 +4,9 @@ const hobby = document.getElementById('hobby');
 
 function buttonEvent() {
     event.preventDefault();
-    console.log(fullName + " " + city + " " + hobby);
+    console.log(fullName.textContent + " " + city.textContent + " " + hobby.textContent);
 
-    if (fullName !== "" || city !== "" || hobby !== "" || fullName, city, hobby === "") {
+    if (fullName === "" || city === "" || hobby === "" || fullName, city, hobby === "") {
         alert("Please fill the following fields");
     } else {
         `Hi ${fullName.textContent}! Welcome to ${city.textContent}. Hope you enjoy ${hobby.textContent} while you're here!`;
